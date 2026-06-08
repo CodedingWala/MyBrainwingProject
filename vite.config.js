1a0d40538252b0af1ae1aa67f163ab1383999ac2
Javed Moinuddin Ansari <ansarijaved092004@gmail.com>
2026-06-08 11:23:29 +0530

    for render

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    chunkSizeWarningLimit: 5000,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('three')) return 'three'
-          if (id.includes('@react-three/fiber')) return 'fiber'
-          if (id.includes('@react-three/drei')) return 'drei'
-          if (id.includes('gsap')) return 'gsap'
-          if (id.includes('node_modules')) return 'vendor'
-        }
-      }
-    }
-  }
+  plugins: [react(),tailwindcss()],
 })
